345436ac031177a89e34cfc8dc2e52e84f49c4ca
pixel <pixel.world7@mail.ru>
2022-03-31 23:28:08 +0300

    rename classes postCard component

--- components/ui/postCard.jsx
+++ components/ui/postCard.jsx
@@ -4,24 +4,24 @@ import Link from "next/link"
 const PostCard = ({ ID, Title, Description, Commented }) => {
 	return (
 		<div className="posts-block__column">
-			<div className="posts-block__card card-posts">
-				<div className="card-posts__wrap-img">
+			<div className="posts-block__card card-post">
+				<div className="card-post__wrap-img">
 					<Link href={`/posts/[idPost]`} as={`/posts/${ID}`}>
-						<a><img className="card-posts__image" src="./imagesPosts/postImage.png" alt="Изображение поста" /></a>
+						<a><img className="card-post__image" src="./imagesPosts/postImage.png" alt="Изображение поста" /></a>
 					</Link>
 				</div>
-				<div className="card-posts__content posts-content-card">
+				<div className="card-post__content post-content-card">
 					<Link href={`/posts/[idPost]`} as={`/posts/${ID}`}>
-						<a><h3 className="posts-content-card__title">{Title}</h3></a>
+						<a><h3 className="post-content-card__title">{Title}</h3></a>
 					</Link>
-					<p className="posts-content-card__text">{Description}</p>
-					<div className="posts-content-card__controllers card-controllers">
+					<p className="post-content-card__text">{Description}</p>
+					<div className="post-content-card__actions card-actions">
 						<Link href={`/posts/[idPost]`} as={`/posts/${ID}`}>
-							<a className="card-controllers__link"><img className="card-controllers__comment-icon" src="./icons/comment.svg" /></a>
+							<a className="card-actions__link"><img alt="Иконка комментария" className="card-actions__comment-icon" src="./icons/comment.svg" /></a>
 						</Link>
-						<span className="card-controllers__number-comments">{Commented.length}</span>
+						<span className="card-actions__number-comments">{Commented.length}</span>
 						<Link href={`/posts/[idPost]`} as={`/posts/${ID}`}>
-							<a className="card-controllers__link"><img className="card-controllers__pencil-icon" src="./icons/pencil.svg" /></a>
+							<a className="card-actions__link"><img alt="Иконка карандаша" className="card-actions__pencil-icon" src="./icons/pencil.svg" /></a>
 						</Link>
 					</div>
 				</div>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,26 @@
 import "../styles/style.scss"
 import { Provider } from "react-redux"
 import createStore from "../store/createStore"
+import Head from "next/head"
+import React from "react"
 
 const store = createStore()
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
-		<div className="wrapper">
-			<Provider store={store}>
-				<Component {...pageProps} />
-			</Provider>
-		</div>
+		<React.Fragment>
+			<Head>
+				<meta charset="utf-8" />
+				<meta name="keywords" content="посты, posts, комментарии, comments" />
+				<meta name="description" content="Приложение FastPost позволяет быстро оставить свой пост и написать комментарии под постами других пользователей." />
+				<title>FastPost</title>
+			</Head>
+			<div className="wrapper">
+				<Provider store={store}>
+					<Component {...pageProps} />
+				</Provider>
+			</div>
+		</React.Fragment>
 	)
 }
 
